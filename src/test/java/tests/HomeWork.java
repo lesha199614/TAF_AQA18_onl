ad722b3bfb717906d6d54adc5e959d7e2f364524
aliakseiv <1189185@gmail.com>
2022-12-26 21:48:28 +0100

    Initial

--- src/test/java/tests/HomeWork.java
+++ src/test/java/tests/HomeWork.java
@@ -3,6 +3,7 @@ package tests;
 import factory.BrowserFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -21,10 +22,18 @@ public class HomeWork {
     }
 
     @Test
-    public void saucedemo(){
+    public void saucedemo() throws InterruptedException {
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.cssSelector("#user-name")).sendKeys("standard_user");
         driver.findElement(By.cssSelector("#password")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        driver.findElement(By.cssSelector("#add-to-cart-sauce-labs-bike-light")).click();
+        driver.findElement(By.cssSelector(".shopping_cart_link")).click();
 
+        //id css
+        Assert.assertEquals(driver.findElement(By.cssSelector("#item_0_title_link")),"Sauce Labs Bike Light");
+
+
+        Thread.sleep(3000);
     }
 }
