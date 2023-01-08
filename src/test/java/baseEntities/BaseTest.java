9d029eb6f84b0ae8bf85c204ffe0718c875768c6
aliakseiv <1189185@gmail.com>
2023-01-08 17:03:56 +0100

    Initial

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -2,6 +2,7 @@ package baseEntities;
 
 import factory.BrowserFactory;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import services.WaitsService;
@@ -11,11 +12,14 @@ public class BaseTest {
     protected WebDriver driver;
     protected UserStep userStep;
     protected WaitsService waitsService;
+    protected Actions actions;
 
     @BeforeMethod
     public void setUp() {
         driver = new BrowserFactory().getDriver();
         waitsService = new WaitsService(driver);
+        actions = new Actions(driver);
+
         //driver.get(ReadProperties.getUrl());
 
         userStep = new UserStep(driver);
