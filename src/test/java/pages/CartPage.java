b291e685c4977b244733af68e00a3deb6b8d5b64
aliakseiv <1189185@gmail.com>
2023-02-13 00:28:38 +0100

    Initial

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -55,7 +55,7 @@ public class CartPage extends BasePage {
     public WebElement getCompleteOrder() {
         return waitsService.waitForExists(completeOrderLocator);
     }
-
+//comment
     public CartPage cartCheckOut() {
         getCheckoutButton().click();
         return this;
